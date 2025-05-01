63af3c5e3003fde61d55841b6bf5aad8a20864b7
Truong Cong Thanh <truongcongthanh2000@gmail.com>
2025-05-01 22:46:38 +0700

    feat: add log config

--- news_trade/config.py
+++ news_trade/config.py
@@ -43,5 +43,5 @@ class Config:
 
     def beautify(self):
         response = vars(self).copy()
-        response["TWITTER_COOKIES_DICT"] = "{.....}"
+        # response["TWITTER_COOKIES_DICT"] = "{.....}"
         return response
