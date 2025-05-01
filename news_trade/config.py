01824d1b1a49debdd372d1552157f43125b91c62
Truong Cong Thanh <truongcongthanh2000@gmail.com>
2025-05-01 22:34:52 +0700

    fix logic flag enable

--- news_trade/config.py
+++ news_trade/config.py
@@ -9,13 +9,13 @@ class Config:
                 "scrape_sleep_time": 600,
                 "sla": "86400",
                 "list_query": [],
-                "enabled": "false"
+                "enabled": False
             },
             "threads": {
                 "list_username": [],
                 "sla": 86400,
                 "scrape_sleep_time": 60,
-                "enabled": "false"
+                "enabled": False
             }
         }
         self.TWITTER_COOKIES_DICT = {}
@@ -27,13 +27,19 @@ class Config:
         self.THREADS_LIST_USERNAME = [thread.strip() for thread in os.environ.get("THREADS_LIST_USERNAME", "").split() if thread.strip()] or config["threads"]["list_username"]
         self.THREADS_SLA = int(os.environ.get("THREADS_SLA") or config["threads"]["sla"])
         self.THREADS_SCRAPE_SLEEP_TIME = int(os.environ.get("THREADS_SCRAPE_SLEEP_TIME") or config["threads"]["scrape_sleep_time"])
-        self.THREADS_ENABLED = str(config["threads"]["enabled"] or os.environ.get("THREADS_ENABLED", "false")).lower() == "true"
+        if "THREADS_ENABLED" in os.environ:
+            self.THREADS_ENABLED = os.environ.get("THREADS_ENABLED").lower == "true"
+        else:
+            self.THREADS_ENABLED = config["threads"]["enabled"]
 
         self.TWITTER_COOKIES_DICT = json.loads(os.environ.get("TWITTER_COOKIES") or config["twitter"]["cookies"])
         self.TWITTER_SCRAPE_SLEEP_TIME = int(os.environ.get("TWITTER_SCRAPE_SLEEP_TIME") or config["twitter"]["scrape_sleep_time"])
         self.TWITTER_LIST_QUERY = [query.strip() for query in os.environ.get("TWITTER_LIST_QUERY", "").split() if query.strip()] or config["twitter"]["list_query"]
         self.TWITTER_SLA = int(os.environ.get("TWITTER_SLA") or config["twitter"]["sla"])
-        self.TWITTER_ENABLED = str(config["twitter"]["enabled"] or os.environ.get("TWITTER_ENABLED", "false")).lower() == "true"
+        if "TWITTER_ENABLED" in os.environ:
+            self.TWITTER_ENABLED = os.environ.get("TWITTER_ENABLED").lower == "true"
+        else:
+            self.TWITTER_ENABLED = config["twitter"]["enabled"]
 
     def beautify(self):
         response = vars(self).copy()
