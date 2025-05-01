028bbf8c117b575b1f07134985c3866e6c10f2d5
Truong Cong Thanh <truongcongthanh2000@gmail.com>
2025-05-01 23:34:14 +0700

    fix log

--- news_trade/config.py
+++ news_trade/config.py
@@ -43,6 +43,6 @@ class Config:
 
     def beautify(self):
         response = vars(self).copy()
-        # response["TWITTER_COOKIES_DICT"] = "{.....}"
-        response["TWITTER_COOKIES_TYPE"] = type(response["TWITTER_COOKIES_DICT"]) is dict
+        response["TWITTER_COOKIES_DICT"] = "{.....}"
+        response["TWITTER_COOKIES_TYPE"] = str(type(response["TWITTER_COOKIES_DICT"]))
         return response
