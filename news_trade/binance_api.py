e33e3068edb6b529c28dee249cdaeb168920789a
Truong Cong Thanh <truongcongthanh2000@gmail.com>
2025-05-12 05:38:52 +0700

    feat: add command info

--- news_trade/binance_api.py
+++ news_trade/binance_api.py
@@ -1,7 +1,19 @@
 from .config import Config
 from .logger import Logger
+import threading
+from contextlib import contextmanager
+from typing import Dict, Set, Tuple
 from .notification import Message
 from binance.client import Client
+
+class BinanceCache:  # pylint: disable=too-few-public-methods
+    _balances: Dict[str, float] = {}
+    _balances_mutex: threading.Lock = threading.Lock()
+
+    @contextmanager
+    def open_balances(self):
+        with self._balances_mutex:
+            yield self._balances
 class BinanceAPI:
     def __init__(self, config: Config, logger: Logger):
         self.config = config
@@ -12,8 +24,22 @@ class BinanceAPI:
             tld=config.COMMAND_TLD
         )
 
+    # spot api
     def get_account(self):
         """
         Get account information
         """
-        return self.binance_client.futures_account_balance()
+        return self.binance_client.get_account(omitZeroBalances="true")
+    
+    def get_ticker_price(self, symbol: str):
+        price = self.binance_client.get_symbol_ticker(symbol=symbol)
+        if price is None or "price" not in price:
+            return 0.0
+        return float(price["price"])
+
+    # future api
+    def get_futures_account(self):
+        return self.binance_client.futures_account()
+    
+    def get_current_position(self):
+        return self.binance_client.futures_position_information()
--- news_trade/command.py
+++ news_trade/command.py
@@ -1,14 +1,16 @@
 from .logger import Logger
 from .config import Config
 from .notification import Message
-from telegram import Update
+from telegram import Update, LinkPreviewOptions
+import telegramify_markdown
+from telegram.constants import ParseMode
 from telegram.ext import Application, CommandHandler, ContextTypes, Updater
 import apprise
 import socket
 import requests
 from .binance_api import BinanceAPI
-import json
 
+EPS = 1e-2
 class Command:
     def __init__(self, config: Config, logger: Logger, binance_api: BinanceAPI):
         self.config = config
@@ -20,7 +22,7 @@ class Command:
         if self.config.COMMAND_ENABLED == False:
             return
         self.application.add_handler(CommandHandler("start", self.start))
-        self.application.add_handler(CommandHandler("account", self.account))
+        self.application.add_handler(CommandHandler("info", self.info))
         self.application.add_error_handler(self.error)
         self.application.run_polling()
 
@@ -38,13 +40,15 @@ class Command:
                 format=apprise.NotifyFormat.TEXT
             ), True)
     
-    async def account(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+    async def info(self, update: Update, context: ContextTypes.DEFAULT_TYPE): # info current spot/future account, ex: balance, pnl, orders, ...
         try:
-            account_info = self.binance_api.get_account()
-            await update.message.reply_text(text=f"Current account info: {json.dumps(account_info, indent=2)}")
+            msg = self.info_spot() + '\n--------------------\n' + self.info_future()
+            msg = telegramify_markdown.markdownify(msg)
+            print(msg)
+            await update.message.reply_text(text=msg, parse_mode=ParseMode.MARKDOWN_V2, link_preview_options=LinkPreviewOptions(is_disabled=True))
         except Exception as err:
             self.logger.error(Message(
-                title=f"Error Command.account - {update}",
+                title=f"Error Command.faccount - {update}",
                 body=f"Error: {err=}", 
                 format=apprise.NotifyFormat.TEXT
             ), True)
@@ -55,3 +59,52 @@ class Command:
             body=f"Error Msg: {context.error}",
             format=apprise.NotifyFormat.TEXT
         ), True)
+
+    def info_spot(self):
+        account_info = self.binance_api.get_account()
+        total_balance = 0.0
+        info = "**SPOT Account**\n"
+        for balance in account_info["balances"]:
+            coin = balance["asset"]
+            free_price = float(balance["free"])
+            locked_price = float(balance["locked"])
+            if free_price + locked_price <= EPS:
+                continue
+            total_balance += free_price + locked_price
+            message = ""
+            if coin == "USDT":
+                message = "USDT: $%.2f" % round(free_price + locked_price, 2)
+            else:
+                message = f"[{coin}](https://www.binance.com/en/trade/{coin}_USDT?type=spot): ${free_price + locked_price:.2f}"
+            message += "\n"
+            info += message
+        info += "\n"
+        info += f"**Total balance**: {total_balance:.2f}"
+        return info
+    
+    def info_future(self):
+        info = "**Future Account**\n"
+        account_info = self.binance_api.get_futures_account()
+        positions = self.binance_api.get_current_position()
+        for position in positions:
+            symbol = position["symbol"]
+            url = f"https://www.binance.com/en/futures/{symbol}"
+            amount = float(position["positionAmt"])
+            if abs(amount) <= EPS: # Open orders
+                continue
+            if amount > 0:
+                position_type = "**BUY**"
+            else:
+                position_type = "**SHORT**"
+            info_position = f"[{symbol}]({url}): {position_type} **{abs(round(float(position['notional']) / float(position['initialMargin'])))}x**, size: **${position['notional']}**, margin: **${position['initialMargin']}**\n"
+            info_position += f"- entryPrice: **${position['entryPrice']}**, marketPrice: **{position['markPrice']}**\n"
+            info_position += f"- PNL: **${float(position['unRealizedProfit']):.2f}**, ROI: **{round(float(position['unRealizedProfit']) / float(position['initialMargin']) * 100.0, 2)}%**\n\n"
+            info += info_position
+
+        info += "\n"
+        info += f"**Before Total Balance**: ${float(account_info['totalWalletBalance']):.2f}\n"
+        info += f"**Total Initial Margin**: ${float(account_info['totalInitialMargin']):.2f} (Position: ${float(account_info['totalPositionInitialMargin']):.2f}, Open: ${float(account_info['totalOpenOrderInitialMargin']):.2f})\n"
+        info += f"**Available Balance**: ${float(account_info['availableBalance']):.2f}\n\n"
+        info += f"**Total Unrealized Profit**: ${float(account_info['totalUnrealizedProfit']):.2f}\n"
+        info += f"**After Total Balance**: ${float(account_info['totalMarginBalance']):.2f}"
+        return info
