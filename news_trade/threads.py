4f40a572183d0416bb1fafd59ad29995313bc6eb
Truong Cong Thanh <truongcongthanh2000@gmail.com>
2025-06-20 20:11:54 +0700

    speed up use playwright in threads

--- news_trade/threads.py
+++ news_trade/threads.py
@@ -54,6 +54,10 @@ class Threads:
             msg = msg.replace('\u25a0', '')
             msg = remove_redundant_spaces(msg)
             self.logger.info(Message(title=f"Playwright installation successful - Time: {datetime.fromtimestamp(int(time.time()), tz=pytz.timezone('Asia/Ho_Chi_Minh'))}", body=msg), False)
+            # start Playwright browser
+            pw = sync_playwright().start()
+            self.browser = pw.chromium.launch(chromium_sandbox=False)
+            self.context = self.browser.new_context(viewport={"width": 1920, "height": 1080})
     
     # Note: we'll also be using parse_thread function we wrote earlier:
 
@@ -115,41 +119,37 @@ class Threads:
             "threads": [],
         }
         try:
-            with sync_playwright() as pw:
-                # start Playwright browser
-                browser = pw.chromium.launch(chromium_sandbox=False)
-                context = browser.new_context(viewport={"width": 1920, "height": 1080})
-                page = context.new_page()
-                url = f'{self.BASE_URL}/@{username}'
+            # start Playwright browser
+            page = self.context.new_page()
+            url = f'{self.BASE_URL}/@{username}'
 
-                page.goto(url)
-                # wait for page to finish loading
-                page.wait_for_selector("[data-pressable-container=true]")
-                # page.wait_for_load_state('load')
-                selector = Selector(page.content())
-                # find all hidden datasets
-                hidden_datasets = selector.css('script[type="application/json"][data-sjs]::text').getall()
-                for hidden_dataset in hidden_datasets:
-                    # skip loading datasets that clearly don't contain threads data
-                    if '"ScheduledServerJS"' not in hidden_dataset:
-                        continue
-                    is_profile = 'follower_count' in hidden_dataset
-                    is_threads = 'thread_items' in hidden_dataset
-                    if not is_profile and not is_threads:
-                        continue
-                    data = json.loads(hidden_dataset)
-                    if is_profile:
-                        user_data = nested_lookup('user', data)
-                        parsed['user'] = self.parse_profile(user_data[0])
-                    if is_threads:
-                        thread_items = nested_lookup('thread_items', data)
-                        threads = [
-                            self.parse_thread(t) for thread in thread_items for t in thread
-                        ]
-                        parsed['threads'].extend(threads)
-                browser.close()
-                return parsed
-            raise Exception(f"Cannot scrape profile threads")
+            page.goto(url)
+            # wait for page to finish loading
+            page.wait_for_selector("[data-pressable-container=true]")
+            # page.wait_for_load_state('load')
+            selector = Selector(page.content())
+            # find all hidden datasets
+            hidden_datasets = selector.css('script[type="application/json"][data-sjs]::text').getall()
+            for hidden_dataset in hidden_datasets:
+                # skip loading datasets that clearly don't contain threads data
+                if '"ScheduledServerJS"' not in hidden_dataset:
+                    continue
+                is_profile = 'follower_count' in hidden_dataset
+                is_threads = 'thread_items' in hidden_dataset
+                if not is_profile and not is_threads:
+                    continue
+                data = json.loads(hidden_dataset)
+                if is_profile:
+                    user_data = nested_lookup('user', data)
+                    parsed['user'] = self.parse_profile(user_data[0])
+                if is_threads:
+                    thread_items = nested_lookup('thread_items', data)
+                    threads = [
+                        self.parse_thread(t) for thread in thread_items for t in thread
+                    ]
+                    parsed['threads'].extend(threads)
+            page.close()
+            return parsed
         except Exception as err:
             self.logger.error(Message(
                 title=f"Error Threads.scrape_profile - username={username}",
